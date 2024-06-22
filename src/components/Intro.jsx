df2215e901b0f2e81158b1e1a6736083e81be4cc
Sarah Shu <sarahjadeshu@gmail.com>
2024-06-21 17:25:28 -0700

    styled intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 function Intro() {
   return (
-    <div className="flex items-center justify-center">Intro</div>
-);
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Sarah</h1>
+      <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
+      <p className="text-sm max-w-xl mb-6">Personal Bio Here</p>
+    </div>
+  );
 }
 
 export default Intro;
