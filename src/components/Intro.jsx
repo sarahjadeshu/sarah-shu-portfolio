182d862577bb103e72f11e4d12d705fd26ce51d6
Sarah Shu <sarahjadeshu@gmail.com>
2024-06-28 19:21:31 -0700

    added data objects for projects

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Intro() {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Sarah</h1>
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Sarah Shu</h1>
       <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
       <p className="text-sm max-w-xl mb-6">Personal Bio Here</p>
     </div>
---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Super Mark-It",
+    imgUrl: "",
+    stack: ["HTML", "SCSS", "JavaScript", "Vite"],
+    link: "",
+  },
+  {
+    title: "Restaurant Reservation App",
+    imgUrl: "",
+    stack: ["HTML", "CSS", "JavaScript", "React"],
+    link: "",
+  },
+  {
+    title: "Super Mark-It",
+    imgUrl: "",
+    stack: ["HTML", "SCSS", "JavaScript"],
+    link: "",
+  }
+];
